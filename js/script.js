69f0b2f2ff96cb05a92ab4ba11065f5241f297fa
Alessandro Liggieri <smmb@live.it>
2020-12-17 16:28:24 +0100

    Esercizio completo

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 for(var i = 1; i <= 100; i++) {
 
- if ( i % 3 === 0) {
-   i = ('fizz');
+ if (i % 3 === 0 && i % 5 === 0) {
+   console.log('FizzBuzz');
  } else if (i % 5 === 0){
-   console.log('Buzz')
- } if (i % 3 === 0 && i % 5 === 0){
-   console.log('FizzBuzz')
+   console.log('Buzz');
+ } else if ( i % 3 === 0){
+   console.log('Fizz');
+ } else {
+   console.log(i);
  }
 
 }
-console.log(i)
